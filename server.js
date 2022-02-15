2ef36935fbd0d48918432cf1e1d7bfc7c056ecc2
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-02-15 17:03:05 -0500

    homepage.handlebars

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
@@ -15,3 +16,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
